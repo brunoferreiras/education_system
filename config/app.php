67a989dc5a66c68592f967c66c207e81e57ea5c7
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-12-23 10:10:09 -0300

    Install bootstrapper and users index

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
         \Kris\LaravelFormBuilder\FormBuilderServiceProvider::class,
+        Bootstrapper\BootstrapperL5ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +228,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'FormBuilder' => \Kris\LaravelFormBuilder\Facades\FormBuilder::class,
+        'Table' => Bootstrapper\Facades\Table::class,
+        'Button' => Bootstrapper\Facades\Button::class,
     ],
 
 ];
