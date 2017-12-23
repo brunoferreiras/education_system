244518841c2589e2db19d9605e748167e48cbac2
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-12-23 12:11:21 -0300

    Insert icons in crud users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -13,8 +13,8 @@
             ->callback('Ações', function($field, $model) {
                 $linkShow = route('admin.users.show', ['user' => $model->id]);
                 $linkEdit = route('admin.users.edit', ['user' => $model->id]);
-                return Button::link('Ver')->asLinkTo($linkShow).'|'.
-                       Button::link('Editar')->asLinkTo($linkEdit);
+                return Button::link(Icon::create('folder-open').'&nbsp;&nbsp;Ver')->asLinkTo($linkShow).'|'.
+                       Button::link(Icon::create('pencil').' Editar')->asLinkTo($linkEdit);
             })
             !!}
         </div>
