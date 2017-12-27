f2531c1eb12f6d6b939ebb461d0e6cd204d347b8
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-12-27 16:21:33 -0300

    Translate faker for pt-BR

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,3 +24,19 @@ $factory->define(\App\Models\User::class, function (Faker $faker) {
         'enrolment' => str_random(6)
     ];
 });
+
+$factory->define(\App\Models\UserProfile::class, function(Faker $faker) {
+    return [
+        'address' => $faker->address,
+        'cep' => function() use($faker) {
+            $cep = preg_replace('/[^0-9]/', '', $faker->postcode());
+            return $cep;
+        },
+        'number' => rand(1,100),
+        'complement' => rand(1,10)%2==0?null:$faker->sentence,
+        'city' => $faker->city,
+        'neighborhood' => $faker->city,
+        'state' => collect(\App\Models\State::$states)->random(),
+    ];
+});
+
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\UserProfile;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,12 +17,16 @@ class UsersTableSeeder extends Seeder
             'email' => 'admin@user.com',
             'enrolment' => 100000
         ])->each(function (User $user) {
+            $profile = factory(UserProfile::class)->make();
+            $user->profile()->create($profile->toArray());
             User::assignRole($user, User::ROLE_ADMIN);
             $user->save();
         });
 
         factory(User::class, 10)->create()->each(function (User $user) {
             if(!$user->userable) {
+                $profile = factory(UserProfile::class)->make();
+                $user->profile()->create($profile->toArray());
                 User::assignRole($user, User::ROLE_TEACHER);
                 User::assignEnrolment(new User(), User::ROLE_TEACHER);
                 $user->save();
@@ -30,6 +35,8 @@ class UsersTableSeeder extends Seeder
 
         factory(User::class, 10)->create()->each(function (User $user) {
             if(!$user->userable) {
+                $profile = factory(UserProfile::class)->make();
+                $user->profile()->create($profile->toArray());
                 User::assignRole($user, User::ROLE_STUDENT);
                 User::assignEnrolment(new User(), User::ROLE_STUDENT);
                 $user->save();
