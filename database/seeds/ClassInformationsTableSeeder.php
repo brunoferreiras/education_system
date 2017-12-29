2bee738723cf2f7f3424d708a335fbad966c666c
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-12-29 17:44:47 -0300

    Create a model for teaching

--- database/seeds/ClassInformationsTableSeeder.php
+++ database/seeds/ClassInformationsTableSeeder.php
@@ -12,12 +12,26 @@ class ClassInformationsTableSeeder extends Seeder
     public function run()
     {
         $students = \App\Models\Subject::all();
+        $teachers = \App\Models\Teacher::all();
+        $subjects = \App\Models\Subject::all();
+
         factory(\App\Models\ClassInformation::class, 50)
             ->create()
-            ->each(function(\App\Models\ClassInformation $model) use ($students) {
+            ->each(function(\App\Models\ClassInformation $model) use ($students, $teachers, $subjects) {
                 /** @var \Illuminate\Support\Collection $studentsCollection */
                 $studentsCollection = $students->random(10);
                 $model->students()->attach($studentsCollection->pluck('id'));
+
+                $teaching = rand(3,6);
+
+                $teachersCol = $teachers->random($teaching);
+                $subjectsCol = $subjects->random($teaching);
+                foreach (range(1, $teaching) as $value) {
+                    $model->teachings()->create([
+                        'subject_id' => $subjectsCol->get($value-1)->id,
+                        'teacher_id' => $teachersCol->get($value-1)->id,
+                    ]);
+                }
             });
     }
 }
